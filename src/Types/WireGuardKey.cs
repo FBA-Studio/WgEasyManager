313c0041c16ebff1985fcf41273fd068f8f3a223
TheLance <98957643+LanceMinecraftFBA@users.noreply.github.com>
2023-04-08 22:32:41 +0300

    Update WireGuardKey.cs

--- src/Types/WireGuardKey.cs
+++ src/Types/WireGuardKey.cs
@@ -3,26 +3,45 @@ using Newtonsoft.Json;
 using Newtonsoft.Json.Converters;
 
 namespace WgEasyManager.Types {
+    ///<summary>
+    /// Info about WireGuard key
+    ///</summary>
     public class WireGuardKey {
+        ///<value>Key ID</value>
         public string Id;
+
+        ///<value>Name of key</value>
         public string Name;
+
+        ///<value>Key status. <b>false</b>, if key is blocked</value>
         public bool Enabled;
+
+        ///<value>IP Adrdress for connection</value>
         public string Address;
+
+        ///<value>Public Key of WG Key</value>
         public string PublicKey;
 
+        ///<value>Date of create</value>
         [JsonConverter(typeof(IsoDateTimeConverter))]
         public DateTime CreatedAt;
 
+        ///<value>Date of last update</value>
         [JsonConverter(typeof(IsoDateTimeConverter))]
         public DateTime UpdatedAt;
 
+        ///<value>Key online status</value>
         [JsonConverter(typeof(StringEnumConverter))]
         public OnlineStatus PersistentKeepAlive;
 
+        ///<value>Latest using</value>
         [JsonConverter(typeof(IsoDateTimeConverter))]
         public DateTime LatestHandshakeAt;
-        
+
+        ///<value></value>
         public long TransferRx;
+
+        ///<value></value>
         public long TransferTx;
     }
 }
