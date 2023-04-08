db2564997050a16721a5f14bc1eaaaf276554561
TheLance <98957643+LanceMinecraftFBA@users.noreply.github.com>
2023-04-08 22:23:57 +0300

    Update WgEasyManager.cs

--- src/WgEasyManager.cs
+++ src/WgEasyManager.cs
@@ -17,19 +17,27 @@ namespace WgEasyManager {
         private static CookieContainer _cookies = new CookieContainer();
         private static CookieCollection cash = new CookieCollection();
         private static readonly string withoutParametr = "none";
-        private static string _password;
-        private static string _serverUrl;
+        private static string _password = "";
+        private static string _serverUrl = "";
+        private static bool HasSsl;
 
-        public WgEasyClient(string password, string serverUrl) {
+        ///<summary>
+        /// Welcome to WgEasyClient!
+        ///</summary>
+        ///<param name="password">The Password for access to your server</param>
+        ///<param name="serverUrl">URL with Port for API Requests</param>
+        ///<param name="hasSsl">If you have SSL - we recommend set <b>true</b>, false - if you hasn't SSL</param>
+        public WgEasyClient(string password, string serverUrl, bool hasSsl) {
             _password = password;
             _serverUrl = serverUrl;
+            HasSsl = hasSsl;
             bool hasSession = loadingSession();
             if(!hasSession) {
                 loging();
             }
         }
 
-        private static bool makeRequest(string method, string urlMethod, string key, string value, out string data) {
+        private bool makeRequest(string method, string urlMethod, string key, string value, out string data) {
             try {
                 HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create(_serverUrl + "/" + urlMethod);
                 httpWebRequest.Method = method;
@@ -47,6 +55,8 @@ namespace WgEasyManager {
                         writer.Write(postDataString);
                     }
                 }
+                if (HasSsl == false)
+                    httpWebRequest.ServerCertificateValidationCallback += (sender, certificate, chain, sslPolicyErrors) => true;
 
                 using(HttpWebResponse httpWebResponse = (HttpWebResponse)httpWebRequest.GetResponse()) {
                     using Stream stream = httpWebResponse.GetResponseStream();
@@ -62,12 +72,15 @@ namespace WgEasyManager {
                 return false;
             }
         }
-        private static bool makeRequest(string method, string urlMethod, out byte[] data) {
+        private bool makeRequest(string method, string urlMethod, out byte[] data) {
             try {
                 HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create(_serverUrl + "/" + urlMethod);
                 httpWebRequest.Method = method;
                 httpWebRequest.CookieContainer = _cookies;
                 httpWebRequest.ContentType = header;
+
+                if (HasSsl == false)
+                    httpWebRequest.ServerCertificateValidationCallback += (sender, certificate, chain, sslPolicyErrors) => true;
 
                 using(HttpWebResponse httpWebResponse = (HttpWebResponse)httpWebRequest.GetResponse()) {
                     using Stream stream = httpWebResponse.GetResponseStream();
@@ -86,7 +99,7 @@ namespace WgEasyManager {
             }
         }
 
-        private static void createSession() {
+        private void createSession() {
             if(!File.Exists(_session_file)) {
                 using(FileStream stream = File.Create(_session_file)) {
                     BinaryFormatter binary = new BinaryFormatter();
@@ -94,7 +107,7 @@ namespace WgEasyManager {
                 }
             }
         }
-        private static bool loadingSession() {
+        private bool loadingSession() {
             if(File.Exists(_session_file)) {
                 using(Stream stream = File.OpenRead(_session_file)) {
                     BinaryFormatter formatter = new BinaryFormatter();
@@ -104,50 +117,98 @@ namespace WgEasyManager {
             }
             return true;
         }
-        private static void updateCookieContainer(CookieCollection cookies) {
+        private void updateCookieContainer(CookieCollection cookies) {
             foreach(Cookie cookie in cookies)
                 _cookies.Add(cookie);
         }
-        private static bool loging() {
+        private bool loging() {
             makeRequest("POST", "api/session", "password", _password, out var temp);
             createSession();
             return true;
         }
 
-        public static List<WireGuardKey> GetKeys() {
+        ///<summary>
+        ///Get list of keys
+        ///</summary>
+        ///<returns><see cref="System.Collections.Generic.List"/> with <see cref="T:WgEasyManager.Types.WireGuardKey"/></returns>
+        public List<WireGuardKey> GetKeys() {
             makeRequest("GET", "api/wireguard/client", "none", "none", out var data);
             return(JObject.Parse(data)[""] as JArray).ToObject<List<WireGuardKey>>();
         }
-        public static bool CreateKey(string name) {
+
+        ///<summary>
+        ///Create new key
+        ///</summary>
+        ///<param name="name">Name of new key</param>
+        public bool CreateKey(string name) {
             makeRequest("POST", "api/wireguard/client", "name", name, out var data);
             return true;
         }
-        public static bool DeleteKey(string clientId) {
+
+        ///<summary>
+        /// Delete key by Client Id
+        ///</summary>
+        ///<param name="clientId">Id of client</param>
+        public bool DeleteKey(string clientId) {
             makeRequest("DELETE", $"api/wireguard/client/{clientId}", withoutParametr, withoutParametr, out var data);
             return true;
         }
-        public static bool UnbanKey(string clientId) {
+
+        ///<summary>
+        ///Unblock key by Client Id
+        ///</summary>
+        ///<param name="clientId">Id of client</param>
+        public bool UnbanKey(string clientId) {
             makeRequest("POST", $"api/wireguard/client/{clientId}/enable", withoutParametr, withoutParametr, out var data);
             return true;
         }
-        public static bool BlockKey(string clientId) {
+
+        ///<summary>
+        ///Block key by Client Id
+        ///</summary>
+        ///<param name="clientId">Id of client</param>
+        public bool BlockKey(string clientId) {
             makeRequest("POST", $"api/wireguard/client/{clientId}/disable", withoutParametr, withoutParametr, out var data);
             return true;
         }
-        public static bool RenameKey(string clientId, string name) {
+
+        ///<summary>
+        ///Rename key by Client Id
+        ///</summary>
+        ///<param name="clientId">Id of client</param>
+        ///<param name="name">New name for key</param>
+        public bool RenameKey(string clientId, string name) {
             makeRequest("PUT", $"api/wireguard/client/{clientId}/name/", "name", name, out var data);
             return true;
         }
-        public static bool SetNewIp(string clientId, string address) {
+
+        ///<summary>
+        ///Set new IP for connection by this key
+        ///</summary>
+        ///<param name="clientId">Id of client</param>
+        ///<param name="address">IP Adress for connection</param>
+        public bool SetNewIp(string clientId, string address) {
             makeRequest("PUT", $"api/wireguard/client/{clientId}/address/", "address", address, out var data);
             return true;
         }
-        public static bool DownloadConfig(string clientId, string path) {
+
+        ///<summary>
+        ///Download .config file for using key in WireGuard Client
+        ///</summary>
+        ///<param name="clientId">Id of client</param>
+        ///<param name="path">Path for saving config file</param>
+        public bool DownloadConfig(string clientId, string path) {
             makeRequest("POST", $"api/wireguard/client/{clientId}/configuration", out var data);
             File.WriteAllBytes($"{path}/{clientId}.config", data);
             return true;
         }
-        public static bool DownloadQrCode(string clientId, string path) {
+        
+        ///<summary>
+        ///Download QR-Code file for using key in WireGuard Mobile Client
+        ///</summary>
+        ///<param name="clientId">Id of client</param>
+        ///<param name="path">Path for saving QR-Code in .svg</param>
+        public bool DownloadQrCode(string clientId, string path) {
             makeRequest("POST", $"api/wireguard/client/{clientId}/qrcode.svg", out var data);
             File.WriteAllBytes($"{path}/{clientId}.svg", data);
             return true;
