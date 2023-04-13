d0a5504a718c98d5b48138394713a83543ddbfc2
TheLance <98957643+LanceMinecraftFBA@users.noreply.github.com>
2023-04-13 16:07:57 +0300

    Create ServerInfo.cs

---
+++ src/Types/ServerInfo.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Net;
+
+namespace WgEasyManager.Types {
+    public class ServerInfo {
+        public string Url;
+        public string Password;
+        public CookieContainer Cookies;
+    }
+}
